20c007b1795bf694f0ac2b01c83c7dd28fab0313
AbarThMarcin <marcin.wesolowski89@gmail.com>
2022-08-22 21:06:11 +0200

    Preparing for deployment

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const otherRoutes = require('./routes/otherRoutes')
 const cors = require('cors')
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 const app = express()
+const path = require('path')
 
 dotenv.config()
 
@@ -22,6 +23,19 @@ connectDB()
 app.use('/api/users', userRoutes)
 app.use('/api/games', gameRoutes)
 app.use('/api/other', otherRoutes)
+
+// Deployment
+__dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, 'client/build')))
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   })
+} else {
+   app.get('/', (req, res) => {
+      res.send('API is running...')
+   })
+}
 
 // Error handlers
 app.use(notFound)
